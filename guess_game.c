e91fdf2892021204a22758e2a8a14f128770ec44
Shravani T V <shravani2201>
2022-12-03 11:53:11 +0530

    my first commit

---
+++ guess_game.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+int main(){
+   int number, guess, nguesses=1;
+   srand(time(0));
+   number = rand()%100+1;
+   printf("The number is %d \n",number);
+   do{
+    printf("Guess the number between 1 to 100\n");
+    scanf("%d", &guess);
+    if(guess>number){
+        printf("Lower nummber plss\n");
+    }
+    else if(guess<number){
+        printf("Higher number plss\n");
+    }
+    else{
+        printf("You guessed it in %d attempts\n",nguesses);
+    }
+    nguesses++;
+   }while(guess!=number);
+   return 0;
+}
